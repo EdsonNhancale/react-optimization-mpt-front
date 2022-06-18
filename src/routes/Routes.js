51d14d52459a48a4269e75d8a01635d67b40f680
Edson Nhancale <edsonnhancale97@gmail.com>
2022-06-18 10:15:43 +0200

    Added code spliting and lazy loading

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import {
     BrowserRouter,
     Routes,
     Route,
 } from "react-router-dom";
-import CookBook from '../pages/CookBook';
-import CookBookLists from '../pages/CookBookLists';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
 
+
+// import CookBook from '../pages/CookBook';
+// import CookBookLists from '../pages/CookBookLists';
+// import Home from '../pages/Home';
+// import Login from '../pages/Login';
+const CookBook = lazy(() => import(/* webpackChunkName: "CookBook" */ '../pages/CookBook'));
+const CookBookLists = lazy(() => import(/* webpackChunkName: "CookBookLists" */ '../pages/CookBookLists'));
+const Home = lazy(() => import(/* webpackChunkName: "Home" */ '../pages/Home'));
+const Login = lazy(() => import(/* webpackChunkName: "Login" */ '../pages/Login'));
 
 export default function RoutesComponent() {
     return (
         <BrowserRouter>
-            <Routes>
-                <>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/book" element={<CookBook />} />
-                    <Route path="/booklists" element={<CookBookLists />} />
-                </>
-            </Routes>
+            <Suspense fallback={<div className="loading">Loading...</div>}>
+                <Routes>
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/book" element={<CookBook />} />
+                        <Route path="/booklists" element={<CookBookLists />} />
+                    </>
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
