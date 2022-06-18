da86334f80f9e9da36df82375c3f73294e295878
Edson Nhancale <edsonnhancale97@gmail.com>
2022-06-18 09:52:24 +0200

    Added coments

--- src/pages/CookBook.js
+++ src/pages/CookBook.js
@@ -19,6 +19,7 @@ export default function CookBook() {
             <AddNewRecipeForm onAddRecipe={addRecipe} />
             <Link to="/booklists">List</Link>
 
+            {"Unnecessary Rendering"}
             <Footer />
         </>
     )
